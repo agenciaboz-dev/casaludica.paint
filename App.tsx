7ea8b41530569c7c329fa00362108764e99c2bbe
nandobfer <nandobfer@yahoo.com.br>
2023-08-22 11:52:03 -0300

    init: paper and navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet } from "react-native"
+import { PaperProvider } from "react-native-paper"
+import { Routes } from "./src/routes"
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    
+
+    return (
+        <PaperProvider>
+            <StatusBar style="auto" />
+            <Routes />
+        </PaperProvider>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
