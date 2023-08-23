2cf14a96f36f0c0fb48624dbef2f88b0c31ec566
tainarasn <tainarasn.sba@gmail.com>
2023-08-23 12:45:52 -0300

    update: home style

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -13,29 +13,35 @@ export const Home: React.FC<HomeProps> = ({ navigation }) => {
     const maxHeight = Dimensions.get("window").height
 
     return (
-        <ImageBackground source={images.background.dots} style={{ zIndex: 0 }}>
-            <View style={{ padding: 20, height: maxHeight, paddingTop: 80 }}>
-                <View style={{ backgroundColor: "transparent", alignItems: "center", gap: 55 }}>
-                    <Image source={images.logo} style={{ width: 250, height: 170 }} />
-                    <Image source={images.theme} style={{ width: 400, height: 250 }} />
+        <ImageBackground source={images.background.dots}>
+            <View style={{ height: maxHeight }}>
+                <View style={{ padding: 20, paddingTop: 70 }}>
+                    <View style={{ alignItems: "center", gap: 55 }}>
+                        <Image source={images.logo} style={{ width: 240, height: 160 }} />
+                        <Image source={images.theme} style={{ width: 390, height: 250 }} />
+                    </View>
                 </View>
-                <View style={{ marginTop: "auto", gap: 15, alignItems: "center", paddingBottom: 15 }}>
-                    <Button
-                        mode="contained"
-                        onPress={() => navigation.navigate(routes.gallery.name)}
-                        textVariant="displayMedium"
-                    >
-                        Catalogo
-                    </Button>
-                    {Platform.OS != "ios" && (
-                        <Button mode="contained" onPress={() => BackHandler.exitApp()} textVariant="headlineSmall">
-                            Sair
-                        </Button>
-                    )}
-                </View>
-            </View>
 
-            <ImageBackground source={images.background.planes}></ImageBackground>
+                <ImageBackground
+                    source={images.background.planes}
+                    style={{ height: 500, width: 420, position: "absolute", top: 440 }}
+                >
+                    <View style={{ gap: 15, alignItems: "center", position: "relative", top: 203 }}>
+                        <Button
+                            mode="contained"
+                            onPress={() => navigation.navigate(routes.gallery.name)}
+                            textVariant="displayMedium"
+                        >
+                            Catalogo
+                        </Button>
+                        {Platform.OS != "ios" && (
+                            <Button mode="contained" onPress={() => BackHandler.exitApp()} textVariant="headlineSmall">
+                                Sair
+                            </Button>
+                        )}
+                    </View>
+                </ImageBackground>
+            </View>
         </ImageBackground>
     )
 }
