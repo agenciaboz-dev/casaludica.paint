01bb646d78758def8f534c84c1cd84c91e79b992
ispadilha <israel.padilha@gmail.com>
2023-08-24 11:57:37 -0300

    Adjustments to ImageContainer

--- src/screens/Gallery/ImageContainer.tsx
+++ src/screens/Gallery/ImageContainer.tsx
@@ -1,7 +1,10 @@
 import { NavigationProp } from "@react-navigation/native"
 import React from "react"
-import { TouchableOpacity, Image } from "react-native"
+import { TouchableOpacity, Image, Dimensions } from "react-native"
 import { routes } from "../../routes"
+
+const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height
 
 interface ImageProps {
     navigation: NavigationProp<any, any>
@@ -10,8 +13,19 @@ interface ImageProps {
 
 export const ImageContainer: React.FC<ImageProps> = ({ navigation, image }) => {
     return (
-        <TouchableOpacity style={{}} onPress={() => navigation.navigate(routes.draw.name, { image })}>
-            <Image source={image} style={{ width: 330, height: 450, resizeMode: "contain", backgroundColor: "white" }} />
+        <TouchableOpacity style={{
+            width: width * 0.9,
+            height: height * 0.6,
+            alignItems: "center",
+            justifyContent: "center",
+            padding: 30,
+            backgroundColor: "white"
+        }} onPress={() => navigation.navigate(routes.draw.name, { image })}>
+            <Image source={image} style={{
+                width: "100%",
+                height: "100%",
+                resizeMode: "contain",
+            }} />
         </TouchableOpacity>
     )
 }
