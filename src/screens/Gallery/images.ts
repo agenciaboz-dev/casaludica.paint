f5e4a1610d684cd80eb31b445739a3f27dc14231
nandobfer <nandobfer@yahoo.com.br>
2023-08-24 13:25:20 -0300

    feat: added real images

--- src/screens/Gallery/images.ts
+++ src/screens/Gallery/images.ts
@@ -1,5 +1,10 @@
 const images = {
     gallery: {
+        [10]: require("../../../assets/gallery/10.png"),
+        [11]: require("../../../assets/gallery/11.png"),
+        [12]: require("../../../assets/gallery/12.png"),
+        [13]: require("../../../assets/gallery/13.png"),
+        [15]: require("../../../assets/gallery/15.png"),
         cachorro: require("../../../assets/gallery/cachorro.png"),
         spiderman: require("../../../assets/gallery/spiderman.jpg"),
         aviao: require("../../../assets/gallery/aviao.jpg"),
